27855f37d6bddec1d84b4a5e99739269c767e1b6
Stevenanwar <fathananwar2115@gmail.com>
2024-10-30 15:41:13 +0700

    Add files via upload

---
+++ tugas ddp5.py
@@ -0,0 +1,44 @@
+#list utama :kendaraan
+kendaraan = ["mio", "motor", "170cc", "biru", "roda 2"]
+
+#mencetak isi dari list kendaraan
+print(kendaraan)
+
+#menambahkan value atau nilai di ujung list (pakai spend())
+
+#proses append 1 (harga kendaraan)
+kendaraan.append("30.000.000")
+
+#proses append 2 (tipe kendaraan)
+kendaraan.append("matic")
+
+#cetak nilai kendaraan setelah perubahan
+print(kendaraan)
+
+#sisipkan nilai untuk tipe kendaraan
+kendaraan.insert(2,"yamaha")
+
+#cetak hasil list akhirnya
+print(kendaraan)#list utama :kendaraan
+kendaraan = ["mio", "motor", "170cc", "biru", "roda 2"]
+
+#mencetak isi dari list kendaraan
+print(kendaraan)
+
+#menambahkan value atau nilai di ujung list (pakai spend())
+
+#proses append 1 (harga kendaraan)
+kendaraan.append("30.000.000")
+
+#proses append 2 (tipe kendaraan)
+kendaraan.append("matic")
+
+#cetak nilai kendaraan setelah perubahan
+print(kendaraan)
+
+#sisipkan nilai untuk tipe kendaraan
+kendaraan.insert(2,"yamaha")
+
+#cetak hasil list akhirnya
+print(kendaraan)
+
